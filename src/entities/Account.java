ed9fdd997adfe4b45a42909f4c561a0f944bb13b
Miguel <rengell.angell@gmail.com>
2024-03-13 13:34:44 -0500

    Creación de atributos, get and set, construtor, toString  de la cuenta

---
+++ src/entities/Account.java
@@ -0,0 +1,57 @@
+package entities;
+
+public class Account {
+    private int id;
+    private String email;
+    private String password;
+    private int rol;
+
+    public Account(int id, String email, String password, int rol) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.rol = rol;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRol() {
+        return rol;
+    }
+
+    public void setRol(int rol) {
+        this.rol = rol;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", rol=" + rol +
+                '}';
+    }
+}
