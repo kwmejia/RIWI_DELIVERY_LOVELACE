6b3562ef1e1b634ebdcd70764ce266c7906dddff
Santi Peláez <santipelaez0@gmail.com>
2024-03-13 13:12:23 -0500

    Tarea finalizada

---
+++ src/Client.java
@@ -0,0 +1,22 @@
+public class Client {
+    private String address;
+
+    public Client(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "address='" + address + '\'' +
+                '}';
+    }
+}
