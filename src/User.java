7f9779d4c9c65325fefe2f4edf87e7a985deca39
Samuel Rodriguez <samuelrodriguez795@gmail.com>
2024-03-13 13:10:06 -0500

    Create Setter an Getter, toStrign, Constructor

--- src/User.java
+++ src/User.java
@@ -1,4 +1,34 @@
 public class User {
+
     private String phone;
     private String name;
+
+    public User(String phone, String name) {
+        this.phone = phone;
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "phone='" + phone + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
