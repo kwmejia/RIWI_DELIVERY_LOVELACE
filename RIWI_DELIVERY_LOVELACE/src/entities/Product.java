e8595c5aef747d533e25e10e31dfb310b26251de
Juan Fernando <59887137+JuanHiguita77@users.noreply.github.com>
2024-03-15 12:20:53 -0500

    Feat: new Auth Controller created

---
+++ RIWI_DELIVERY_LOVELACE/src/entities/Product.java
@@ -0,0 +1,58 @@
+package entities;
+
+public class Product {
+    private int id;
+    private double price;
+    private String name;
+    private String category;
+
+    public Product(int id, double price, String name, String category) {
+        this.id = id;
+        this.price = price;
+        this.name = name;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id ="+ id +
+                "price=" + price +
+                ", name='" + name +
+                ", category='" + category + '\'' +
+                '}';
+    }
+}
+
